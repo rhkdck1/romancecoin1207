a4cbdfdc5224ca90ebc8c2dc72ee3f310e1ddfd7
rhkdck1 <40975867+rhkdck1@users.noreply.github.com>
2019-12-23 15:23:51 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4666,14 +4666,14 @@ void static RomancecoinMiner(CWallet *pwallet)
         loop
         {
             unsigned int nHashesDone = 0;
-            printf("hashTarget = : %d \n", hashTarget);  
+            //printf("hashTarget = : %u \n", hashTarget);  
 
             uint256 thash;
             char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
             loop
             {
                 scrypt_1024_1_1_256_sp(BEGIN(pblock->nVersion), BEGIN(thash), scratchpad);
-                printf("thash = :  %d \n", thash);            
+                //printf("thash = :  %u \n", thash);            
 
                 if (thash <= hashTarget)
                 {
